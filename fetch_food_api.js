fb819b7c86ed69b3d1048118651e9e792a98f591
RedaAitOuahmed <reda.aitouahmed@gmail.com>
2018-10-26 11:51:08 +0200

    added function to call search
    We can search an ingredient from another asunc function

--- fetch_food_api.js
+++ fetch_food_api.js
@@ -1,26 +1,33 @@
-function search_ingredient(ingredient) {
+async function search_ingredient(ingredient) {
     var ingredients = [];
-    fetch('https://world.openfoodfacts.org/cgi/search.pl?search_terms=' + ingredient + '&search_simple=1&action=process&json=1&page_size=1000')
-        .then(function (response) {
-            response.json().then(function (data) {
-                for (var i = 0; i < data.products.length; i++) {
-                    if (data.products[i].product_name.includes(ingredient)) {
-                        ingredients.push(data.products[i]);
-                        /*var listItem = document.createElement('li');
-                        listItem.innerHTML = data.products[i].product_name;
-                        myList.appendChild(listItem);*/
-                    }
-                }
-                return ingredients;
-            })
+    return fetch('https://world.openfoodfacts.org/cgi/search.pl?search_terms=' + ingredient + '&search_simple=1&action=process&json=1&page_size=1000')
+        .then(function (response)
+        {
+            return response.json()
         })
+        .then(function (data) {
+            for (var i = 0; i < data.products.length; i++) {
+                if (data.products[i].product_name.includes(ingredient))
+                {
+                    ingredients.push(data.products[i]);
+                }
+            }
+            return Promise.resolve(ingredients);
+        })
+
 }
 
-var product = "egg";
-
-function submitForm() {
-    var ingredient = document.getElementById("ingredient").value;
-    console.log( search_ingredient('lait'));
+async function wait_for_me(prod){
+    return fetch('https://world.openfoodfacts.org/cgi/search.pl?search_terms=' + 'milk' + '&search_simple=1&action=process&json=1&page_size=1000')
+        .then(function (resp) {return resp.json() });
+    return Promise.resolve('heyyyy' + prod);
+}
+async function submitForm() {
+    let ingredient = document.getElementById("ingredient").value;
+    products = await search_ingredient(ingredient);
+    for (var i = 0; i < products.length; i++){
+        console.log(products[i].product_name)
+    }
 
 }
 
