26584421b990b737cdce6a2e23d7e71d2732c020
manon-dekyvere <44434925+manon-dekyvere@users.noreply.github.com>
2018-12-20 12:00:50 +0100

    Added description to the project.

--- fetch_food_api.js
+++ fetch_food_api.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 async function submitForm() {
     var ingredient = document.getElementById("ingredient").value;
@@ -15,15 +16,48 @@ async function search_ingredient(ingredient) {
                         ingredients.push(data.products[i]);
 
                     }
-                }
-                return ingredients;
-            })
+=======
+async function search_ingredient(ingredient) {
+    var ingredients = [];
+    return fetch('https://world.openfoodfacts.org/cgi/search.pl?search_terms=' + ingredient + '&search_simple=1&action=process&json=1&page_size=1000')
+        .then(function (response)
+        {
+            return response.json()
         })
+        .then(function (data) {
+            for (var i = 0; i < data.products.length; i++) {
+                if (data.products[i].product_name.includes(ingredient))
+                {
+                    ingredients.push(data.products[i]);
+>>>>>>> 0d08977a15d39233ccac8b1525f393ecbd519d39
+                }
+            }
+            return Promise.resolve(ingredients);
+        })
+<<<<<<< HEAD
 }
 console.log(await search_ingredient('lait'));
+=======
+>>>>>>> 0d08977a15d39233ccac8b1525f393ecbd519d39
 
-var product = "egg";
+}
 
+<<<<<<< HEAD
+=======
+async function wait_for_me(prod){
+    return fetch('https://world.openfoodfacts.org/cgi/search.pl?search_terms=' + 'milk' + '&search_simple=1&action=process&json=1&page_size=1000')
+        .then(function (resp) {return resp.json() });
+    return Promise.resolve('heyyyy' + prod);
+}
+async function submitForm() {
+    let ingredient = document.getElementById("ingredient").value;
+    products = await search_ingredient(ingredient);
+    for (var i = 0; i < products.length; i++){
+        console.log(products[i].product_name)
+    }
+
+}
+>>>>>>> 0d08977a15d39233ccac8b1525f393ecbd519d39
 
 /*
     .then(function(data) {
